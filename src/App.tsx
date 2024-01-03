f3df91ef981412108a98456582eb3d03e2c5e92a
ryouhei0622 <seijakunomutou@gmail.com>
2024-01-03 12:20:08 +0900

    feat: Rowの定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Row} from './components/Row';
+import {requests} from './request';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+<div className="App">
+    <Row
+      title="NETFLIX ORIGINALS"
+      fetchUrl={requests.feachNetflixOriginals}
+      isLargeRow
+    />
+    <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+    <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+    <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+    <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+    <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+    <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
+</div>
+
   );
 }
 
---
+++ src/request.js
@@ -0,0 +1,15 @@
+
+// const API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = "0836bf9f52576953b740ca158f109e29";
+console.log("これはAPI keyの値です",API_KEY);
+
+export const requests ={
+    feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    feachNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
+    feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    feactHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
+}
